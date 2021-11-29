23ef31c955d64f4e488a91ca104e189254a9741b
Alvaro Avilés Redondo <alvaro.avilesr@alumnos.upm.es>
2021-11-29 19:01:02 +0100

    Cambiado el nombre de las pruebas para que sea mas descriptivo.

--- src/test/java/com/practica/cajablanca/PruebasMayorLongitud.java
+++ src/test/java/com/practica/cajablanca/PruebasMayorLongitud.java
@@ -9,8 +9,8 @@ public class PruebasMayorLongitud {
     Editor editor = new Editor();
 
     @Test
-    @DisplayName("MayorLongitudPrueba1")
-    public void MayorLongitudPrueba1() throws EmptyCollectionException {
+    @DisplayName("MayorLongitudPruebaCamino1")
+    public void MayorLongitudPruebaCamino1() throws EmptyCollectionException {
         String h = editor.mayorLongitud();
         assertNull(h);
     }
@@ -19,24 +19,24 @@ public class PruebasMayorLongitud {
     /* La prueba 3 no se puede probar porque el tamaño del editor tiene que ser minimo 1 y para que se salga del for tendría que ser 0, por lo que itera obligatoriamente dentro del for */
 
     @Test
-    @DisplayName("MayorLongitudPrueba4")
-    public void MayorLongitudPrueba4() throws EmptyCollectionException {
+    @DisplayName("MayorLongitudPruebaCamino4")
+    public void MayorLongitudPruebaCamino4() throws EmptyCollectionException {
         editor.leerFichero("ficheros/PruebasMayorLongitud-Prueba4.txt");
         String h = editor.mayorLongitud();
         assertEquals("hola",h);
     }
 
     @Test
-    @DisplayName("MayorLongitudPrueba5")
-    public void MayorLongitudPrueba5() throws EmptyCollectionException {
+    @DisplayName("MayorLongitudPruebaCamino5")
+    public void MayorLongitudPruebaCamino5() throws EmptyCollectionException {
         editor.leerFichero("ficheros/PruebasMayorLongitud-Prueba5.txt");
         String h = editor.mayorLongitud();
         assertEquals("adios",h);
     }
 
     @Test
-    @DisplayName("MayorLongitudPrueba6")
-    public void MayorLongitudPrueba6() throws EmptyCollectionException {
+    @DisplayName("MayorLongitudPruebaCamino6")
+    public void MayorLongitudPruebaCamino6() throws EmptyCollectionException {
         editor.leerFichero("ficheros/PruebasMayorLongitud-Prueba6.txt");
         String h = editor.mayorLongitud();
         assertEquals("adios",h);
--- src/test/java/com/practica/cajablanca/PruebasNumPalabras.java
+++ src/test/java/com/practica/cajablanca/PruebasNumPalabras.java
@@ -13,27 +13,27 @@ public class PruebasNumPalabras {
     Editor prueba = new Editor();
 
     @Test
-    @DisplayName("NumPalabrasPrueba1")
-    public void NumPalabrasPrueba1() {
+    @DisplayName("NumPalabrasPruebaCamino1")
+    public void NumPalabrasPruebaCamino1() {
         assertThrows(IllegalArgumentException.class, () -> prueba.numPalabras(0,prueba.size()-1,"hola"));
 
     }
     @Test
-    @DisplayName("NumPalabrasPrueba2")
-    public void NumPalabrasPrueba2() {
+    @DisplayName("NumPalabrasPruebaCamino2")
+    public void NumPalabrasPruebaCamino2() {
         assertThrows(IllegalArgumentException.class, () -> prueba.numPalabras(3,prueba.size()+1,"hola"));
     }
 
     @Test
-    @DisplayName("NumPalabrasPrueba3")
-    public void NumPalabrasPrueba3() {
+    @DisplayName("NumPalabrasPruebaCamino3")
+    public void NumPalabrasPruebaCamino3() {
         int h = prueba.numPalabras(2,prueba.size()-1,"hola");
         assertEquals(0,h);
     }
 
     @Test
-    @DisplayName("NumPalabrasPrueba4")
-    public void NumPalabrasPrueba4() {
+    @DisplayName("NumPalabrasPruebaCamino4")
+    public void NumPalabrasPruebaCamino4() {
         prueba.leerFichero("ficheros/PruebasNumPalabras-Prueba4.txt");
         int h = prueba.numPalabras(1,0,"hola");
         assertEquals(0,h);
@@ -43,16 +43,16 @@ public class PruebasNumPalabras {
         vale 1 y this.lista no puede estar vacia */
 
     @Test
-    @DisplayName("NumPalabrasPrueba6")
-    public void NumPalabrasPrueba6() {
+    @DisplayName("NumPalabrasPruebaCamino6")
+    public void NumPalabrasPruebaCamino6() {
         prueba.leerFichero("ficheros/PruebasNumPalabras-Pruebas6y7.txt");
         int h = prueba.numPalabras(1,2,"x");
         assertEquals(0,h);
     }
 
     @Test
-    @DisplayName("NumPalabrasPrueba7")
-    public void NumPalabrasPrueba7() {
+    @DisplayName("NumPalabrasPruebaCamino7")
+    public void NumPalabrasPruebaCamino7() {
         prueba.leerFichero("ficheros/PruebasNumPalabras-Pruebas6y7.txt");
         int h = prueba.numPalabras(1,2,"hola");
         assertEquals(1,h);
